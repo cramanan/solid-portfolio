b641962ed6befb5708eff3ec6587c0a8708f3c92
cramanan <cramananjaonapro@gmail.com>
2025-01-13 16:16:00 +0100

    style:edited header

--- public/preload.js
+++ public/preload.js
@@ -2,12 +2,5 @@ try {
     const regex = new RegExp("(^|; )settings=([^;]*)");
     const value = document.cookie.match(regex);
     const parsed = JSON.parse(value[2]);
-    if (
-        parsed &&
-        typeof parsed === "object" &&
-        "dark" in parsed &&
-        typeof parsed.dark === "boolean" &&
-        parsed.dark
-    )
-        document.documentElement.classList.add("dark");
+    if (parsed.dark) document.documentElement.classList.add("dark");
 } catch {}
