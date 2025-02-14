4f94ee45a0b9f08f28bef5f90cef81cba740326a
cramanan <cramananjaonapro@gmail.com>
2025-02-14 15:19:28 +0100

    chore: removed useless div

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,8 @@ import { useAppState } from "~/AppContext";
 export default function Footer() {
     const { t } = useAppState();
     return (
-        <footer>
-            <div class="text-center py-1">
-                Cyril Ram. © 2025 | {t("copyrights")}
-            </div>
+        <footer class="text-center py-1">
+            Cyril Ram. © 2025 | {t("copyrights")}.
         </footer>
     );
 }
