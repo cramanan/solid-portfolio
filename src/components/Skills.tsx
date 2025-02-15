b5ff702543ec96c0d8ee50208635d32dc648efd3
cramanan <cramananjaonapro@gmail.com>
2025-02-15 15:28:07 +0100

    feat: improve Skills component layout and add icon rendering

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,28 @@
+import { For } from "solid-js";
+import Rust from "./icons/Rust";
+import React from "./icons/React";
+import Solid from "./icons/Solid";
+import Golang from "./icons/Golang";
 import { useAppState } from "~/AppContext";
+
+const icons = [{ icons: [Rust, Golang] }, { icons: [React, Solid] }];
 
 export default function Skills() {
     const { t } = useAppState();
     return (
-        <section id="skills" class="h-screen">
-            {t("Skills")}
+        <section class="lg:h-screen py-14" id="skills">
+            <div class="container mx-auto px-4">
+                <h2 class="text-4xl font-bold text-center lg:text-start mb-8">
+                    {t("Skills")}
+                </h2>
+                <For each={icons}>
+                    {(item) => (
+                        <For each={item.icons}>
+                            {(Icon) => <Icon width={44} />}
+                        </For>
+                    )}
+                </For>
+            </div>
         </section>
     );
 }
