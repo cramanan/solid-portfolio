47b5017695c8f70f607e448aa9446cfca8b87eb2
cramanan <cramananjaonapro@gmail.com>
2025-02-15 15:26:14 +0100

    feat: add Footer component to RootLayout and remove from Home

--- src/app.tsx
+++ src/app.tsx
@@ -4,12 +4,14 @@ import { ParentProps } from "solid-js";
 import Header from "~/components/Header";
 import { AppContextProvider } from "./AppContext";
 import "./app.css";
+import Footer from "./components/Footer";
 
 function RootLayout(props: ParentProps) {
     return (
         <AppContextProvider>
             <Header />
             {props.children}
+            <Footer />
         </AppContextProvider>
     );
 }
