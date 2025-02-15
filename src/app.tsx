ce230405493486431c2d0601e755d8a358b5381a
cramanan <cramananjaonapro@gmail.com>
2025-02-15 12:22:21 +0100

    chore: removed unnecessary Suspense

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,23 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { ParentProps, Suspense } from "solid-js";
+import { ParentProps } from "solid-js";
 import Header from "~/components/Header";
 import { AppContextProvider } from "./AppContext";
-import { MetaProvider } from "@solidjs/meta";
 import "./app.css";
 
 function RootLayout(props: ParentProps) {
     return (
         <AppContextProvider>
             <Header />
-            <Suspense>{props.children}</Suspense>
+            {props.children}
         </AppContextProvider>
     );
 }
 
 export default function App() {
     return (
-        <MetaProvider>
-            <Router explicitLinks root={RootLayout}>
-                <FileRoutes />
-            </Router>
-        </MetaProvider>
+        <Router explicitLinks root={RootLayout}>
+            <FileRoutes />
+        </Router>
     );
 }
