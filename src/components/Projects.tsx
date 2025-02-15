6e408194de5fa6c515edfc6f2f28568685dea62f
cramanan <cramananjaonapro@gmail.com>
2025-02-15 15:27:49 +0100

    feat: enhance Projects component layout and styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -32,23 +32,25 @@ const projects: Project[] = [
 export default function Projects() {
     const { t } = useAppState();
     return (
-        <section class="h-screen py-12" id="projects">
+        <section class="lg:h-screen py-14" id="projects">
             <div class="container mx-auto px-4">
-                <h2 class="text-4xl font-bold text-center mb-8 dark:text-white">
+                <h2 class="text-4xl font-bold text-center lg:text-start mb-8">
                     {t("Projects")}
                 </h2>
                 <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     <For each={projects}>
                         {(project) => (
-                            <li class="overflow-hidden hover:shadow-lg transition-shadow duration-300 p-4">
-                                <h2>{project.title}</h2>
+                            <li class="overflow-hidden shadow-sm hover:shadow-lg transition-shadow duration-300 p-4">
+                                <h2 class="text-lg font-semibold">
+                                    {project.title}
+                                </h2>
                                 <p class="text-sm">{project.description}</p>
                                 <div class="flex justify-between py-4">
                                     <a
                                         href={project.projectUrl}
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        class="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3"
+                                        class="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background h-9 rounded-md px-3"
                                     >
                                         <Github width={20} />
                                         <span>Source Code</span>
