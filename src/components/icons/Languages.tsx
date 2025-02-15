7dc070bd7fed592f4454676bdd7963fa8bf750de
cramanan <cramananjaonapro@gmail.com>
2025-02-15 12:23:24 +0100

    chore: added basic SVG props to Language component

--- src/components/icons/Languages.tsx
+++ src/components/icons/Languages.tsx
@@ -1,10 +1,12 @@
-export default function Languages(props: { class: string }) {
+import { JSX } from "solid-js";
+
+export default function Languages(props: JSX.SvgSVGAttributes<SVGSVGElement>) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            class={props.class}
+            {...props}
         >
             <path
                 fill-rule="evenodd"
