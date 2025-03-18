ab7f0fa11f1ff5ed26d6343c72310c833a2b62f3
cramanan <cramananjaonapro@gmail.com>
2025-03-18 10:39:19 +0100

    style: added icons to skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,49 +6,56 @@ import React from "./icons/React";
 import Solid from "./icons/Solid";
 import TypeScript from "./icons/TypeScript";
 import Tauri from "./icons/Tauri";
-import Bullseye from "./icons/Bullseye";
-import Gauge from "./icons/Gauge";
+import Wasm from "./icons/WASM";
+
+function Bar() {
+    return <>Bar</>;
+}
 
 function Tabs() {
     return (
         <div class="w-full h-full flex items-center justify-center">
-            <div class="rounded-sm border-[1.5px] w-[209px] h-full">
-                <div class="flex border-b-1 h-3 items-center justify-end gap-1 px-1">
-                    <span class="h-2 w-2 border-1 rounded-full bg-green-400"></span>
-                    <span class="h-2 w-2 border-1 rounded-full bg-yellow-400"></span>
-                    <span class="h-2 w-2 border-1 rounded-full bg-red-400"></span>
+            <div class="rounded-[6px] border-[1.5px] w-[200px] h-full">
+                <div class="flex border-b-1 h-3 items-center gap-1 px-1">
+                    <span class="h-1.5 w-1.5 border-1 rounded-full bg-green-400"></span>
+                    <span class="h-1.5 w-1.5 border-1 rounded-full bg-red-400"></span>
+                    <span class="h-1.5 w-1.5 border-1 rounded-full bg-yellow-400"></span>
                 </div>
-                <div class="p-1 relative">
-                    <div class="border w-10 h-10 mx-1.5"></div>
-                </div>
+                <div class="p-1 relative"></div>
             </div>
+            {/* <div class="rounded-[6px] border-[1.5px] w-[50px] h-[80px]">
+                <div class="p-1 relative"></div>
+            </div> */}
         </div>
     );
+}
+
+function Baz() {
+    return <>Baz</>;
 }
 
 const icons = [
     {
         Component: Tabs,
         icons: [
-            { title: "Rust", Icon: Rust },
-            { title: "Golang", Icon: Golang },
-        ],
-    },
-    {
-        Component: Tabs,
-        icons: [
             { title: "React", Icon: React },
-            { title: "Solid", Icon: Solid },
+            { title: "SolidJS", Icon: Solid },
             { title: "TypeScript", Icon: TypeScript },
         ],
     },
-    { Component: Tabs, icons: [{ title: "Tauri", Icon: Tauri }] },
+    {
+        Component: Bar,
+        icons: [
+            { title: "Rust", Icon: Rust },
+            { title: "Golang", Icon: Golang },
+            { title: "WebAssembly", Icon: Wasm },
+        ],
+    },
+    {
+        Component: Baz,
+        icons: [{ title: "Tauri", Icon: Tauri }],
+    },
 ];
-
-const cards = [
-    { Icon: Bullseye, label: "Precision" },
-    { Icon: Gauge, label: "Efficiency" },
-] as const;
 
 export default function Skills() {
     const { t } = useAppState();
@@ -81,27 +88,6 @@ export default function Skills() {
                                         )}
                                     </For>
                                 </div>
-                            </li>
-                        )}
-                    </For>
-                </ul>
-                <ul class="flex flex-col lg:flex-row lg:justify-evenly py-5">
-                    <For each={cards}>
-                        {(item) => (
-                            <li class="lg:w-1/3">
-                                <div class="flex items-center gap-2 justify-center m-1.5">
-                                    <item.Icon width={50} />
-                                    <h1 class="text-3xl font-semibold">
-                                        {t(item.label)}
-                                    </h1>
-                                </div>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur,
-                                    adipisicing elit. Eos eum illum illo magnam
-                                    in aspernatur animi, earum itaque
-                                    repudiandae ducimus ullam saepe iure ut
-                                    laborum adipisci quidem at maiores ex?
-                                </p>
                             </li>
                         )}
                     </For>
