7f38b535630233ea29735006b5ecbf5d84899b6f
cramanan <cramananjaonapro@gmail.com>
2025-03-18 10:38:31 +0100

    #

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import Skills from "~/components/Skills";
 import Contact from "~/components/Contact";
 import HeroBanner from "~/components/HeroBanner";
 import Projects from "~/components/Projects";
+import About from "~/components/About";
 
 export default function Home() {
     return (
         <>
             <HeroBanner />
+            <About />
             <Skills />
             <Projects />
             <Contact />
