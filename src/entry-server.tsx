dcb796619be54a10216bc36b8ec4af7180d66e65
cramanan <cramananjaonapro@gmail.com>
2025-03-18 13:17:28 +0100

    feat: added resources preloads

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -13,6 +13,19 @@ export default createHandler(() => (
                     />
                     <link rel="icon" href="/favicon.ico" />
                     <script src="/preload.js"></script>
+                    <link
+                        rel="preload"
+                        href="/assets/fonts/NeueMontreal-Bold.woff"
+                        as="font"
+                        type="font/woff"
+                    />
+                    <link
+                        rel="preload"
+                        href="/assets/fonts/NeueMontreal-Regular.woff"
+                        as="font"
+                        type="font/woff"
+                    />
+                    <link rel="preload" as="image" href="/picture.webp" />
                     {assets}
                 </head>
                 <body>
