a9761b292da31b689017e954997a4dda18afa18c
cramanan <cramananjaonapro@gmail.com>
2025-03-19 16:30:00 +0100

    chore: removed skills translations

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,38 +3,20 @@ import Bullseye from "./icons/Bullseye";
 import Gauge from "./icons/Gauge";
 import { useAppState } from "~/AppContext";
 
-const cards = [
-    { Icon: Bullseye, label: "precision" },
-    { Icon: Gauge, label: "efficiency" },
-] as const;
-
 export default function About() {
     const { translation } = useAppState();
     return (
-        <section id="about" class="py-20 px-4 lg:h-screen">
+        <section id="about" class="lg:h-screen py-20 px-4">
             <div class="mx-auto px-4">
-                <h2 class="text-4xl font-bold text-center lg:text-start mb-8">
+                <h2 class="text-5xl font-bold text-center lg:text-start mb-8">
                     {translation.headers.about()}
                 </h2>
-                <ul class="flex flex-col lg:flex-row lg:justify-evenly py-5">
-                    <For each={cards}>
-                        {(item) => (
-                            <li class="lg:w-1/3">
-                                <div class="flex items-center gap-2 justify-center m-1.5">
-                                    <item.Icon width={50} />
-                                    <h1 class="text-3xl font-semibold">
-                                        {translation.skills[item.label].title()}
-                                    </h1>
-                                </div>
-                                <p>
-                                    {translation.skills[
-                                        item.label
-                                    ].description()}
-                                </p>
-                            </li>
-                        )}
-                    </For>
-                </ul>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Architecto dicta itaque modi magni. Nemo quaerat harum
+                    tenetur quidem. Id ad hic et eligendi quisquam, quasi
+                    deleniti repudiandae maiores modi perspiciatis!
+                </p>
             </div>
         </section>
     );
